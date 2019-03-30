39d32f45dbe2c9b0021011d56a33f71cd9931989
Laura Schmidt <lmrussell28@gmail.com>
2019-03-30 13:49:11 -0500

    Push Rankings file to github

---
+++ Team roster/team_roster.js
@@ -0,0 +1,18 @@
+
+
+
+var access_level = 'trial'
+var version = 'v4'
+var language_code = 'en'
+var team_id = 
+var format = 'json'
+var api_key = 'dzsdr8tp6f3sxcmwuzacchwn'
+
+
+var baseUrl = `https://api.sportradar.us/ncaamb/trial/v4/en/teams/c10544de-e3bd-4776-ba2e-83df8c017fd1/profile.json?api_key=htjv47kv2e8vn933n49vjp9j`
+
+
+console.log(baseUrl)
+
+
+
